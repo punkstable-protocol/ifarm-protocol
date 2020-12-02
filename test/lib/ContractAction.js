f4880151bcc3bdbea277d0ba847f87dc41d05f40
breeze <ts001_breeze@space-p.com>
2020-12-03 00:00:47 +0800

    update rebaser test case

--- test/lib/ContractAction.js
+++ test/lib/ContractAction.js
@@ -1,6 +1,7 @@
 const UniswapV2Router02 = require("../contractsJson/UniswapV2Router02.json");
 const UniswapV2Factory = require("../contractsJson/UniswapV2Factory.json");
 const iTokenDelegator = require("../contractsJson/iTokenDelegator.json");
+const Rebaser = require("../contractsJson/iTokenRebaser.json")
 const wETH = require("../contractsJson/wETH.json");
 const IFA = require("../../build/contracts/IFAToken.json");
 
@@ -24,6 +25,9 @@ class ContractAt {
     }
     IFA(_contractAddress) {
         return this.at(IFA.abi, _contractAddress);
+    }
+    Rebaser(_contractAddress){
+        return this.at(Rebaser.abi, _contractAddress);
     }
 }
 
